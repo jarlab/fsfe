72886775bd6e2be2d60ca82d9e1020f637181f53
Balraj Jha Anand <balraj.anand.20.2@mountblue.tech>
2022-10-01 17:43:48 +0530

    add custom header and meme staus code

--- app.js
+++ app.js
@@ -6,4 +6,13 @@ app.get('/', (req, res) => {
 	res.send('Hello World! - Balraj');
 });
 
+app.get('/demo', (req, res) => {
+	// custom header
+	res.set('X-full-stack', '4life');
+	// status code
+	res.status(418);
+	// response back
+	res.send('I prefer coffee');
+});
+
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
